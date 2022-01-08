dbb45b9709dc0914b89f72e7cb7d673557ec98e5
George Mamadashvili <georgemamadashvili@gmail.com>
2022-01-08 20:09:54 +0400

    Inline components

--- src/edit.js
+++ src/edit.js
@@ -1,16 +1,17 @@
 /**
  * WordPress dependencies
  */
+import { __, _x } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
+import { Button, Placeholder, Spinner, ToolbarGroup } from '@wordpress/components';
+import { BlockControls, BlockIcon } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
  */
-import Controls from './components/controls';
-import Loading from './components/loading';
 import Preview from './components/preview';
-import Placeholder from './components/placeholder';
 import fetchBookmark from './api';
+import icon from './icon';
 
 export default function Edit( {
 	attributes,
@@ -44,26 +45,56 @@ export default function Edit( {
 	}
 
 	if ( fetching ) {
-		return <Loading />;
+		return (
+			<div className="wp-block-embed is-loading">
+				<Spinner />
+				<p>{ __( 'Loading', 'bookmark-card' ) }</p>
+			</div>
+		);
 	}
 
 	const showPlaceholder = ! attributes.title || editingUrl;
 	if ( showPlaceholder ) {
 		return (
 			<Placeholder
-				onSubmit={ onSubmit }
-				value={ url }
-				onChange={ ( event ) => setUrl( event.target.value ) }
-			/>
+				icon={ <BlockIcon icon={ icon } /> }
+				label={ __( 'Site URL', 'bookmark-card' ) }
+				className="wp-block-embed"
+				instructions={ __(
+					'Enter URL to convert into a bookmark card.',
+					'bookmark-card'
+				) }
+			>
+				<form onSubmit={ onSubmit }>
+					<input
+						type="url"
+						value={ url || '' }
+						className="components-placeholder__input"
+						aria-label={ __( 'Site URL', 'bookmark-card' ) }
+						placeholder={ __( 'Enter URL here…', 'bookmark-card' ) }
+						onChange={ ( event ) => setUrl( event.target.value) }
+					/>
+					<Button isPrimary type="submit">
+						{ _x( 'Submit', 'button label', 'bookmark-card' ) }
+					</Button>
+				</form>
+			</Placeholder>
 		);
 	}
 
+	const toolbarControls = [
+		{
+			icon: 'edit',
+			title: __( 'Edit URL', 'bookmark-card' ),
+			onClick: () => setEditingUrl( true ),
+		},
+	];
+
 	return (
 		<>
-			<Controls
-				showEditButton={ url }
-				switchBackToURLInput={ () => setEditingUrl( true ) }
-			/>
+			<BlockControls>
+				<ToolbarGroup controls={ toolbarControls } />
+			</BlockControls>
 			<Preview
 				bookmark={ attributes }
 				className={ className }
