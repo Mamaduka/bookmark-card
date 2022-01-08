498a27da1bd139914ff3521d390973d9ec0bc164
George Mamadashvili <georgemamadashvili@gmail.com>
2022-01-08 19:13:24 +0400

    Update block registration

--- bookmark-card.php
+++ bookmark-card.php
@@ -4,7 +4,7 @@
  * Plugin URI:        https://github.com/Mamaduka/bookmark-card
  * Description:       Turn any URL into a beautiful preview card.
  * Version:           1.0.0
- * Requires at least: 5.4
+ * Requires at least: 5.8
  * Requires PHP:      5.6
  * Author:            George Mamadashvili
  * Author URI:        https://mamaduka.com/
@@ -19,6 +19,15 @@ namespace Mamaduka\BookmarkCard;
 const PLUGIN_DIR  = __DIR__;
 const PLUGIN_FILE = __FILE__;
 
-require_once PLUGIN_DIR . '/inc/block.php';
 require_once PLUGIN_DIR . '/inc/bookmark.php';
 require_once PLUGIN_DIR . '/inc/rest-route.php';
+
+/**
+ * Registers the block and required assets.
+ *
+ * @return void
+ */
+function register_block() {
+	register_block_type( __DIR__ );
+}
+add_action( 'init', __NAMESPACE__ . '\\register_block' );
