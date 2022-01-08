76cd53761d322ad7d7cf6f66a77c931e65c033cc
George Mamadashvili <georgemamadashvili@gmail.com>
2022-01-08 19:34:18 +0400

    Convert Preview into functional component

--- src/components/preview.js
+++ src/components/preview.js
@@ -1,50 +1,33 @@
 /**
  * WordPress dependencies
  */
-import { Component } from '@wordpress/element';
+import { useEffect, useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import Card from './card';
 
-export default class Preview extends Component {
-	constructor() {
-		super( ...arguments );
-		this.hideOverlay = this.hideOverlay.bind( this );
-		this.state = {
-			interactive: false,
-		};
-	}
+export default function Preview( { bookmark, className, isSelected } ) {
+	const [ interactive, setInteractive ] = useState( false );
 
-	static getDerivedStateFromProps( nextProps, state ) {
-		if ( ! nextProps.isSelected && state.interactive ) {
-			return { interactive: false };
+	useEffect( () => {
+		if ( ! isSelected && interactive ) {
+			setInteractive( false );
 		}
+	}, [ isSelected, interactive ] );
 
-		return null;
-	}
-
-	hideOverlay() {
-		this.setState( { interactive: true } );
-	}
-
-	render() {
-		const { bookmark, className } = this.props;
-		const { interactive } = this.state;
-
-		/* eslint-disable jsx-a11y/no-static-element-interactions */
-		return (
-			<>
-				<Card { ...bookmark } className={ className } />
-				{ ! interactive && (
-					<div
-						className="block-library-embed__interactive-overlay"
-						onMouseUp={ this.hideOverlay }
-					/>
-				) }
-			</>
-		);
-		/* eslint-enable jsx-a11y/no-static-element-interactions */
-	}
+	/* eslint-disable jsx-a11y/no-static-element-interactions */
+	return (
+		<>
+			<Card { ...bookmark } className={ className } />
+			{ ! interactive && (
+				<div
+					className="block-library-embed__interactive-overlay"
+					onMouseUp={ () => setInteractive( true ) }
+				/>
+			) }
+		</>
+	);
+	/* eslint-enable jsx-a11y/no-static-element-interactions */
 }
